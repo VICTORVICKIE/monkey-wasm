90d6548622169dd9193f91972c02c1f8a00feca5
VICTORVICKIE <s.vickie14@gmail.com>
2023-06-17 09:53:29 +0530

    feat: rows of tokens

--- src/lib.rs
+++ src/lib.rs
@@ -8,11 +8,22 @@ use wasm_bindgen::prelude::*;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+fn rows_of_tokens(tokens: Vec<Token>) -> Vec<Vec<Token>> {
+    return tokens.into_iter().fold(Vec::new(), |mut rows, token| {
+        let idx = token.position.line - 1;
+        if idx >= rows.len() {
+            rows.resize_with(idx + 1, Vec::new);
+        }
+        rows[idx].push(token);
+        rows
+    });
+}
 
 #[wasm_bindgen]
 pub fn lexer(input: String) -> Result<JsValue, JsValue> {
     let lexer = Lexer::new(input);
     let tokens: Vec<Token> = lexer.into();
+    let rows = rows_of_tokens(tokens);
 
-    return Ok(serde_wasm_bindgen::to_value(&tokens)?);
+    return Ok(serde_wasm_bindgen::to_value(&rows)?);
 }
